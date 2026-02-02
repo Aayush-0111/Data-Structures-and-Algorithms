aa8752cf38abcbd8d9e2437b77e11803036141b3
Aayush Mishra <aayushmishra249@gmail.com>
2026-02-02 10:07:39 +0530

    Time: 0 ms (100%), Space: 8.6 MB (50%) - LeetHub

--- 3827-count-monobit-integers/3827-count-monobit-integers.cpp
+++ 3827-count-monobit-integers/3827-count-monobit-integers.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int countMonobit(int n) {
-        if(n == 0) return 1;
-        long long pow = 1;
-        int cnt = 0;
-        while(n >= pow){
-            cnt++;
-            pow *= 2;
-            if(n == pow-1) return cnt+1;
-        }
-        return cnt;
+        return __lg(n+1) + 1;
     }
 };
