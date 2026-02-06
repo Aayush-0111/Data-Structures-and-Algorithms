8fd5f57b225c364435c5d18b123c8085d2572663
Aayush Mishra <aayushmishra249@gmail.com>
2026-02-06 23:35:51 +0530

    Time: 27 ms (87.5%), Space: 105.1 MB (52.91%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int minRemoval(vector<int>& nums, int k) {
+        // think about which elements to keep
+        // in a sorted array, mini will be at the staring and maxi at the end
+        // we use two pointers and traverse the array to find the maximum length 
+        // segment of array where the condition holds i.e., mini*k >= maxi
+        // we expand till the right pointer reach an element strictly greater than the 
+        // k*mini or till it goes out of bounds. 
+        // We return the elements which are not in the segment i.e n - (j-i+1).
         int n = nums.size();
         if(n == 1) return 0;
-        int i = 0, ans = n;
+        int i = 0, j = 1, ans = n;
         sort(nums.begin(),nums.end());
-        for(int i = 0; i < n; i++){
-            long long val = 1LL*k*nums[i];
-            int j = upper_bound(nums.begin(),nums.end(),val) - nums.begin();
-            ans = min(ans, n - (j - i));
+        while(i < n){
+            // keep moving right untill right is invalid(cond. breaks)
+            while(j < n && 1LL*nums[i]*k >= nums[j]) j++;
+            ans = min(ans,n-(j-i));
+            i++;
         }
         return ans;
     }
