19d41872342b739d0d00b1ff0521b0bdb54cb202
Aayush Mishra <aayushmishra249@gmail.com>
2026-02-12 21:06:44 +0530

    Time: 113 ms (80.18%), Space: 25.2 MB (89.33%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int n = s.size();
+        int maxi = 0;
+        for(int i = 0; i < n; i++){
+            vector<int> mp(26,0);
+            for(int j = i; j < n; j++){
+                mp[s[j]-'a']++;
+                int prev = -1;
+                bool flag = true;
+                for(int x = 0; x < 26; x++){
+                    if(mp[x] == 0) continue;
+                    else if(prev == -1) prev = mp[x];
+                    else if(prev != mp[x]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(!flag) continue;
+                maxi = max(maxi,j-i+1);
+            }
+        }
+        return maxi;
+    }
+};
