9cda5bb46d1bdf4238bda8079d494e6d22f2053f
Aayush Mishra <aayushmishra249@gmail.com>
2026-02-16 10:00:28 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.07%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -3,9 +3,9 @@ public:
     int reverseBits(int n) {
         bitset<32> x(n);
         for(int i = 0; i < 16; i++){
-            bool temp = x[i];
-            x[i] = x[31-i];
-            x[31-i] = temp;
+            x[i] = x[i]^x[31-i];
+            x[31-i] = x[i]^x[31-i];
+            x[i] = x[i]^x[31-i];
         }
         unsigned long long y = x.to_ullong();
         return (int)y;
