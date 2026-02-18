de797f054191aa5790c5d088edc02ff026b24740
Aayush Mishra <aayushmishra249@gmail.com>
2026-02-18 18:36:46 +0530

    Time: 0 ms (100%), Space: 7.7 MB (91.43%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        bitset<32> x(n);
-        string num = x.to_string();
-        std::size_t found = num.find_first_not_of("0");
-        for(size_t i = found; i < (int)num.size(); i++){
-            if(num[i] == num[i-1]) return false;
-        }
-        return true;
+        unsigned int v = (unsigned int)n + ((unsigned int)n >> 1);
+        return ((v & (v+1)) == 0) ? true : false;
     }
 };
