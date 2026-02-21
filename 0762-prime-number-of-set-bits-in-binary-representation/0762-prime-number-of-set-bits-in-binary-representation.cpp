ec2d2355c0aec1c66ef8cbf2efbc99c900dcec55
Aayush Mishra <aayushmishra249@gmail.com>
2026-02-21 09:59:39 +0530

    Time: 153 ms (6.85%), Space: 51.8 MB (5.09%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,20 +1,9 @@
 class Solution {
-private:
-    vector<bool> sieve(int n){
-        vector<bool> prime(n+1,true);
-        for(int p = 2; p*p <= n; p++){
-            if(prime[p]){
-                for(int i = p*p; i <= n; i+=p){
-                    prime[i] = false;
-                }
-            }
-        }
-        return prime;
-    }
 public:
     int countPrimeSetBits(int left, int right) {
-        vector<bool> prime = sieve(right);
-        prime[1] = false; 
+        // maximum bits required to represent 10^6 is 20 (1000000)
+        // maximum number of set bits that can be prime is 19.
+        int prime[8] = {2,3,5,7,11,13,17,19}; 
         int ans = 0;
         for(int i = left; i <= right; i++){
             int cnt = 0;
@@ -23,8 +12,12 @@ public:
             for(int j = 0; j < 32; j++){
                 if(s[j] == '1') cnt++;
             }
-            cout << s << "\n";
-            if(prime[cnt]) ans++;
+            for(int j = 0; j < 8; j++){
+                if(prime[j] == cnt){
+                    ans++;
+                    break;
+                }
+            }
         }
         return ans;
     }
