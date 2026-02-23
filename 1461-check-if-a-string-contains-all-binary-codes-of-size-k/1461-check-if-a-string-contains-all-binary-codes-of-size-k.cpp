2fd7aaa280a227ad5d6b7180ac53ad45ac11d122
Aayush Mishra <aayushmishra249@gmail.com>
2026-02-23 23:02:34 +0530

    Time: 14 ms (92.75%), Space: 23.2 MB (96.92%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -8,7 +8,7 @@ public:
         int mask = 0;
         int full_mask = total - 1; // k set bits
         vector<bool> seen(total,false);
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; ++i){
             mask = ((mask << 1) & full_mask) | (s[i]-'0');
             if(i >= k-1 && !seen[mask]){
                 seen[mask] = true;
