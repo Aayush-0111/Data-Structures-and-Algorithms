7bc7548c69cc5597f93b8bd5d8a529d019b61b76
Aayush Mishra <aayushmishra249@gmail.com>
2026-02-26 20:03:47 +0530

    Time: 0 ms (100%), Space: 9.5 MB (18.91%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    string add1toBinary(string s){
+    void add1toBinary(string& s){
         int n = s.size()-1;
         while(n >= 0 && s[n] == '1'){
             s[n] = '0'; 
@@ -12,7 +12,6 @@ private:
             // 1 overflowed, so add it in front
             s = '1' + s;
         }
-        return s;
     }
 public:
     int numSteps(string s) {
@@ -21,7 +20,7 @@ public:
             if(s.back() != '1'){
                 s.pop_back();
             }else{
-                s = add1toBinary(s);
+                add1toBinary(s);
             }
             count++;
         }
