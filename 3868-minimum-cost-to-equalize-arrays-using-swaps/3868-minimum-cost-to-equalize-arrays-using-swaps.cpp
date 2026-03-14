12ad6da1f6aff33b9cae0acd9173b8dfa2f0bb4e
Aayush Mishra <aayushmishra249@gmail.com>
2026-03-14 23:31:20 +0530

    Time: 143 ms (90.53%), Space: 254.6 MB (97.05%) - LeetHub

--- 3868-minimum-cost-to-equalize-arrays-using-swaps/3868-minimum-cost-to-equalize-arrays-using-swaps.cpp
+++ 3868-minimum-cost-to-equalize-arrays-using-swaps/3868-minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -3,8 +3,8 @@ public:
     int minCost(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int> mp;
         int cost = 0;
-        for(int i : nums1) mp[i]++;
-        for(int i : nums2) mp[i]--;
+        for(int &i : nums1) mp[i]++;
+        for(int &i : nums2) mp[i]--;
         for(auto& p : mp){
             if(abs(p.second)%2 != 0) return -1;
             if(p.second > 0) cost += p.second/2;
