8de8893b57150e2a1608ea1c5aa7ceb886fb8560
Aayush Mishra <aayushmishra249@gmail.com>
2026-03-14 11:05:18 +0530

    Time: 7 ms (50.45%), Space: 18 MB (44.04%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     vector<string> output;
-    void backTrack(string& curr, int n){
+    int x;
+    void backTrack(string& curr, int n, int k){
         if((int)curr.size() == n){
             output.push_back(curr);
+            x++;
             return;
         }
-
+        if(x == k) return;
         for(char c : {'a','b','c'}){
             // no two adjacent elements should be same
             if(!curr.empty() && curr.back() == c) continue;
             curr.push_back(c);
-            backTrack(curr,n);
+            backTrack(curr,n,k);
             curr.pop_back();
         }
     }
     string getHappyString(int n, int k) {
+        x = 0;
         string curr = "";
-        backTrack(curr,n);
+        backTrack(curr,n,k);
         return ((k <= (int)output.size()) ? output[k-1] : "");
     }
 };
