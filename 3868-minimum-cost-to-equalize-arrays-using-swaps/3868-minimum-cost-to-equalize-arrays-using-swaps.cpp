8a0d87caad1d5112510cc32e90f546f7de096736
Aayush Mishra <aayushmishra249@gmail.com>
2026-03-14 23:30:48 +0530

    Time: 136 ms (94%), Space: 254.7 MB (88.24%) - LeetHub

--- 3868-minimum-cost-to-equalize-arrays-using-swaps/3868-minimum-cost-to-equalize-arrays-using-swaps.cpp
+++ 3868-minimum-cost-to-equalize-arrays-using-swaps/3868-minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     int minCost(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        unordered_set<int> st;
-        bool flag = true;
-        unordered_map<int,int> frq,cnt1,cnt2;
-        for(int i = 0; i < n; i++){
-            st.insert(nums1[i]);
-            cnt1[nums1[i]]++;
-            cnt2[nums2[i]]++;
-            frq[nums1[i]]++;
-            frq[nums2[i]]++;
-            if(nums1[i] != nums2[i]) flag = false;
-        }
-        if(flag) return 0;
-        for(auto& p : frq){
-            if(p.second%2 != 0) return -1;
-        }
+        unordered_map<int,int> mp;
         int cost = 0;
-        for(auto i : st){
-            cost += ((cnt1[i] - frq[i]/2 > 0) ? cnt1[i]-frq[i]/2 : 0);
+        for(int i : nums1) mp[i]++;
+        for(int i : nums2) mp[i]--;
+        for(auto& p : mp){
+            if(abs(p.second)%2 != 0) return -1;
+            if(p.second > 0) cost += p.second/2;
         }
         return cost;
     }
