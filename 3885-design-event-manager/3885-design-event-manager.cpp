135e690ed7de28400080205d41817cea6686322d
Aayush Mishra <aayushmishra249@gmail.com>
2026-03-29 20:47:08 +0530

    Time: 145 ms (86.7%), Space: 292.5 MB (96.95%) - LeetHub

--- 3885-design-event-manager/3885-design-event-manager.cpp
+++ 3885-design-event-manager/3885-design-event-manager.cpp
@@ -1,28 +1,23 @@
 class EventManager {
 public:
-    struct cmp{
-        bool operator()(const pair<int,int>& a, const pair<int,int>& b){
-            if(a.first == b.first) return a.second > b.second;
-            return a.first < b.first;
-        }
-    };
-    priority_queue<pair<int,int>,vector<pair<int,int>>,cmp> pq;
+    priority_queue<pair<int,int>,vector<pair<int,int>>> pq;
     unordered_map<int,int> mp;
     EventManager(vector<vector<int>>& events) {
         for(int i = 0; i < (int)events.size(); i++){
             mp[events[i][0]] = events[i][1];
-            pq.push(make_pair(events[i][1],events[i][0]));
+            pq.push(make_pair(events[i][1],-events[i][0]));
         }
     }
     
     void updatePriority(int eventId, int newPriority) {
-        pq.push(make_pair(newPriority,eventId));
+        pq.push(make_pair(newPriority,-eventId));
         mp[eventId] = newPriority;
     }
     
     int pollHighest() {
         while(!pq.empty()){
             auto [priority,event] = pq.top();
+            event = -event;
             pq.pop();
             if(mp.count(event) && priority == mp[event]){
                 mp.erase(event);
