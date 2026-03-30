b66068a8bb41ad1086833a4d4b90387795dfca50
Aayush Mishra <aayushmishra249@gmail.com>
2026-03-30 09:23:06 +0530

    Time: 4 ms (83.33%), Space: 20.2 MB (52.87%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -3,7 +3,7 @@ public:
     bool checkStrings(string s1, string s2) {
         // positions for which j-i is even can be odd-odd and even-even
         int n = s1.size();
-        vector<int> vo1(26,0),vo2(26,0),ve1(26,0),ve2(26,0);
+        int vo1[26] = {0}, vo2[26] = {0}, ve1[26] = {0}, ve2[26] = {0};
         for(int i = 0; i < n; i++){
             if(i%2 == 0){
                 ve1[s1[i]-'a']++;
@@ -13,6 +13,6 @@ public:
                 vo2[s2[i]-'a']++;
             }
         }
-        return ((ve1 == ve2) && (vo1 == vo2));
+        return ((equal(ve1,ve1+26,ve2)) && (equal(vo1,vo1+26,vo2)));
     }
 };
