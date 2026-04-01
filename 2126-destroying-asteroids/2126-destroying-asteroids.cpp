284fcedd31cabf17513b1c2f4c0df767823b4623
Aayush Mishra <aayushmishra249@gmail.com>
2026-04-01 22:32:17 +0530

    Time: 29 ms (72.99%), Space: 109 MB (6.44%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        sort(asteroids.begin(),asteroids.end());
+        priority_queue<int,vector<int>,greater<int>> pq;
         int n = asteroids.size();
         long long newMass = mass;
         for(int i = 0; i < n; i++){
-            if(newMass < asteroids[i]) return false;
-            else {
+            if(newMass < asteroids[i]){
+                pq.push(asteroids[i]);
+            }else{
                 newMass += asteroids[i];
+            }
+        }
+        while(!pq.empty()){
+            if(pq.top() > newMass) return false;
+            else {
+                newMass += pq.top();
+                pq.pop();
             }
         }
         return true;
