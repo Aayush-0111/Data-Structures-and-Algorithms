3cb5515256c5494a94fecae1ea9bd11075826c7c
Aayush Mishra <aayushmishra249@gmail.com>
2026-04-02 23:20:34 +0530

    Time: 11 ms (57.39%), Space: 19.2 MB (75.16%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
     int countCollisions(string directions) {
-        int s = 0, r = 0, ans = 0;
-        for(char& c : directions){
-            if(c == 'R'){
-                r++;
-                s = 0;
-            }else if(c == 'S'){
-                if(r != 0){
-                    ans += r;
-                    r = 0;
-                }
-                s++;
-            }else {
-                if(s != 0){
-                    ans += 1;
-                }else if(r != 0){
-                    ans += 2;
-                    r--;
-                    s++;
-                    if(r != 0){
-                        ans += r;
-                        r = 0;
-                    }
-                }
-            }
+        int n = directions.size(), ans = 0;
+        int l = 0, r = n-1;
+        while(l < n && directions[l] == 'L') l++;
+        while(r >= 0 && directions[r] == 'R') r--;
+        // key insight is that
+        // cars which are in the middle and not stationary
+        // will definitely collide
+        // if they collide with a 'S', we count that car
+        // or if they collide with another moving car, we are already counting both of them
+        // so contributing +2(as asked in question). ex : 'R' -> 'L' and vide-versa
+        for(int i = l; i <= r; i++){
+            if(directions[i] != 'S') ans++;
         }
         return ans;
     }
