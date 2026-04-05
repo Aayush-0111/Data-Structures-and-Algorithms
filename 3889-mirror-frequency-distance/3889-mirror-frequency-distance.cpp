c274031e2865efc8b9cf1d9b3b600de0ac413a9f
Aayush Mishra <aayushmishra249@gmail.com>
2026-04-05 23:05:45 +0530

    Time: 117 ms (38.48%), Space: 57.9 MB (98.42%) - LeetHub

--- 3889-mirror-frequency-distance/3889-mirror-frequency-distance.cpp
+++ 3889-mirror-frequency-distance/3889-mirror-frequency-distance.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int mirrorFrequency(string s) {
         int n = s.size(), sum = 0;
-        vector<int> frq1(26);
-        vector<int> frq2(10);
+        int frq1[26];
+        int frq2[10];
         for(char& c : s){
            if(isalpha(c)) frq1[c-'a']++;
            else frq2[c-'0']++;
