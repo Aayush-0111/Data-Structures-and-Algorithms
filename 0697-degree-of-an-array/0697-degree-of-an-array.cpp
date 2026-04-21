a386ea751eaba26960a6cc2a4ffc090f7910be2d
Aayush Mishra <aayushmishra249@gmail.com>
2026-04-22 01:37:39 +0530

    Time: 12 ms (49.31%), Space: 30.3 MB (13.02%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.cpp
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
         int n = nums.size(), maxi = 0, mini = n;
-        unordered_map<int,tuple<int,int,int>> mp;
+        unordered_map<int,int> left, right, count;
         for(int i = 0; i < n; i++){
-            if(!mp.count(nums[i])){
-                get<1>(mp[nums[i]]) = i;
+            count[nums[i]]++;
+            if(!left.count(nums[i])){
+                left[nums[i]] = i;
             }
-            get<0>(mp[nums[i]])++;
-            get<2>(mp[nums[i]]) = i;
-            maxi = max(maxi,get<0>(mp[nums[i]]));
+            maxi = max(maxi,count[nums[i]]);
         }
-        vector<int> mps;
-        for(auto& [m,p] : mp){
-            if(get<0>(p) == maxi){
-                int s = get<1>(p), e = get<2>(p);
-                mini = min(mini,e-s+1);
+        for(int i = n-1; i >= 0; i--){
+            if(!right.count(nums[i])){
+                right[nums[i]] = i;
+            }
+        }
+        for(int i : nums){
+            if(count[i] == maxi){
+                mini = min(mini,right[i]-left[i] + 1);
             }
         }
         return mini;
