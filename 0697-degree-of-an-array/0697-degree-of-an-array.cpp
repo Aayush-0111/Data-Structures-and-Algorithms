62eef253f8f90b8f6d1edcb76680eaa2f063ccad
Aayush Mishra <aayushmishra249@gmail.com>
2026-04-22 02:02:21 +0530

    Time: 3 ms (95.25%), Space: 28.3 MB (99.34%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.cpp
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -4,21 +4,14 @@ public:
         int n = nums.size(), maxi = 0, mini = n;
         int left[50000], right[50000], count[50000] = {0};
         fill(left, left + 50000, -1);
-        fill(right,right + 50000, -1);
         for(int i = 0; i < n; i++){
-            count[nums[i]]++;
             if(left[nums[i]] == -1) left[nums[i]] = i;
-            maxi = max(maxi,count[nums[i]]);
-        }
-        for(int i = n-1; i >= 0; i--){
-            if(right[nums[i]] == -1){
-                right[nums[i]] = i;
-            }
-        }
-        for(int i : nums){
-            if(count[i] == maxi){
-                mini = min(mini,right[i]-left[i] + 1);
-            }
+            if(++count[nums[i]] > maxi){
+                maxi = count[nums[i]];
+                mini = i - left[nums[i]] + 1;
+            }else if(count[nums[i]] == maxi){
+                mini = min(mini,i-left[nums[i]]+1);
+            } 
         }
         return mini;
     }
