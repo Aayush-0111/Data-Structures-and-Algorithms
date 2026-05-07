cbb147a751ce5ba6a3626ce1eb0753695954699b
Aayush Mishra <aayushmishra249@gmail.com>
2026-05-07 21:51:00 +0530

    Time: 0 ms (100%), Space: 8 MB (37.26%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int binaryGap(int n) {
+        string s = bitset<32>(n).to_string();
+        s = s.substr(s.find_first_not_of("0"));
         int l = -1, maxi = 0;
-        for(int i = 0; i < 32; i++){
-            // check whether current bit is 1
-            if((n >> i) & 1 > 0){
-                if(l != -1){
-                    maxi = max(maxi,i-l);
-                }
+        for(int i = 0; i < (int)s.size(); i++){
+            if(s[i] == '1' && l == -1) l = i;
+            else if(s[i] == '1'){
+                maxi = max(maxi,abs(l-i));
                 l = i;
             }
         }
