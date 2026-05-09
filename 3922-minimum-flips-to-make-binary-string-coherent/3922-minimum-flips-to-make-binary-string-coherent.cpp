2cbb32064b49991a3ed001a457edf627cafc7ae0
Aayush Mishra <aayushmishra249@gmail.com>
2026-05-09 23:14:09 +0530

    Time: 4 ms (81.4%), Space: 19.6 MB (55.29%) - LeetHub

--- 3922-minimum-flips-to-make-binary-string-coherent/3922-minimum-flips-to-make-binary-string-coherent.cpp
+++ 3922-minimum-flips-to-make-binary-string-coherent/3922-minimum-flips-to-make-binary-string-coherent.cpp
@@ -6,7 +6,7 @@ public:
         int ones = 0, zeroes = 0;
         for(char& c : s){
             if(c == '0') zeroes++;
-            else ones++;
+            ones = n - zeroes;
         }
         if(s[0] == '1' && s[n-1] == '1') return min(zeroes,ones-2);
         return (ones > 0) ? min(zeroes,ones-1) : 0;
