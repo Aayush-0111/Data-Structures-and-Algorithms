abd7cd4ebe876a3c7cd4e976b1fd0b005fda87e6
Aayush Mishra <aayushmishra249@gmail.com>
2026-05-09 23:09:06 +0530

    Time: 0 ms (100%), Space: 19.7 MB (55.29%) - LeetHub

---
+++ 3922-minimum-flips-to-make-binary-string-coherent/3922-minimum-flips-to-make-binary-string-coherent.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minFlips(string s) {
+        int n = s.size();
+        if(n < 3) return 0;
+        int ones = 0, zeroes = 0;
+        for(char& c : s){
+            if(c == '0') zeroes++;
+            else ones++;
+        }
+        if(s[0] == '1' && s[n-1] == '1') return min(zeroes,ones-2);
+        return (ones > 0) ? min(zeroes,ones-1) : 0;
+    }
+};
