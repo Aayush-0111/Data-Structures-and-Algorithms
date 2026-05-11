4134eea721f6a31faa635185c523255a927f63d6
Aayush Mishra <aayushmishra249@gmail.com>
2026-05-11 10:55:30 +0530

    Time: 0 ms (100%), Space: 14.3 MB (83.77%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -1,6 +1,6 @@
 int tens[6] = {1,10,100,1000,10000,100000};
 class Solution {
-private:
+public:
     vector<int> ans;
     inline void extract(int num){
         int md = upper_bound(tens,tens+6,num) - tens - 1;
@@ -10,7 +10,6 @@ private:
             num = r;
         }
     }
-public:
     vector<int> separateDigits(vector<int>& nums) {
         int n = nums.size();
         ans.reserve(n*6);
