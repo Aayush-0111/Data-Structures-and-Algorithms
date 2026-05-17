10e9d88a3fd7f787bb16b2938c2bebd8da708273
Aayush Mishra <aayushmishra249@gmail.com>
2026-05-17 11:42:12 +0530

    Time: 0 ms (100%), Space: 35.3 MB (79.07%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,15 +1,7 @@
 class Solution {
 private:
-    void adjList(vector<vector<int>>& adj, vector<int>& nums){
-        int n = adj.size();
-        for(int i = 0; i < n; i++){
-            // left neighbour
-            adj[i][0] = (i-nums[i] >= 0) ? i-nums[i] : -1; 
-            // right neighbour
-            adj[i][1] = (i+nums[i] < n) ? i+nums[i] : -1;
-        }
-    }
-    bool bfs(vector<vector<int>>& adj, vector<int>& arr, vector<bool>& visited, int node){
+    bool bfs(vector<int>& arr, vector<bool>& visited, int node){
+        int n = arr.size();
         queue<int> q;
         q.push(node);
         visited[node] = true;
@@ -17,12 +9,15 @@ private:
             int front = q.front();
             q.pop();
             if(arr[front] == 0) return true;
-            for(int i : adj[front]){
-                if(i == -1) continue;
-                if(!visited[i]){
-                    visited[i] = true;
-                    q.push(i);
-                }
+            int l = (front-arr[front] >= 0) ? front-arr[front] : -1;
+            int r = (front+arr[front] <= n-1) ? front+arr[front] : -1;
+            if(l != -1 && !visited[l]){
+                visited[l] = true;
+                q.push(l);
+            }
+            if(r != -1 && !visited[r]){
+                visited[r] = true;
+                q.push(r);
             }
         }
         return false;
@@ -30,14 +25,11 @@ private:
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
-        vector<vector<int>> adj(n,vector<int>(2,0));
-        // preparing adj list
-        adjList(adj,arr);
         // every element(i)
         //   /       |
         //  i-1      i+1
         // create visited
         vector<bool> visited(n,false);
-        return bfs(adj,arr,visited,start);
+        return bfs(arr,visited,start);
     }
 };
