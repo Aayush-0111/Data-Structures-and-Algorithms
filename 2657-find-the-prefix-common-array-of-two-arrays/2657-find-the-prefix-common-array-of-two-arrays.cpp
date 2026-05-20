a7f4e74f4640bacb5daa3ae98ffdf7b60b159f36
Aayush Mishra <aayushmishra249@gmail.com>
2026-05-20 09:54:33 +0530

    Time: 1 ms (77.24%), Space: 85.8 MB (76.5%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,8 +1,8 @@
+bool check1[51] = {false}, check2[51] = {false};
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = A.size(), cnt = 0;
-        vector<bool> check1(n+1,false), check2(n+1,false);
         vector<int> ans(n,0);
         ans[n-1] = n;
         for(int i = 0; i < n-1; i++){
@@ -16,6 +16,8 @@ public:
             check1[A[i]] = true;
             check2[B[i]] = true;
         }
+        memset(check1, false, sizeof(check1));
+        memset(check2, false, sizeof(check2));
         return ans;
     }
 };
