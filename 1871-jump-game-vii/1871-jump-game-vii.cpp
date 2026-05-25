7d6de61d6f5b176fc533d0a95bf888b4d36fd395
Aayush Mishra <aayushmishra249@gmail.com>
2026-05-25 23:46:01 +0530

    Time: 20 ms (15.06%), Space: 24.7 MB (40.96%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    vector<bool> visited;
+private:
+    bool bfs(string& s, vector<bool>& visited, int node, int minJump, int maxJump){
+        int n = s.size();
+        queue<int> q;
+        visited[node] = true;
+        q.push(node);
+        int farthest = -1;
+        while(!q.empty()){
+            int f = q.front();
+            q.pop();
+            if(f == n-1) return true;
+            // we are using farthest so that we don't revisit a range
+            // farhtest makes sure how far we have scanned the array
+            // visited is essentially doing the same thing but because
+            // we are rechecking the overlapping ranges, time limit exceeds.
+            int start = max(f+minJump,farthest+1);
+            for(int j = start; j <= min(f+maxJump,n-1); j++){
+                if(s[j] == '0' && !visited[j]){
+                    q.push(j);
+                    visited[j] = true;
+                }
+                farthest = j;
+            }
+        }
+        return false;
+    }
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+        visited.resize(n,false);
+        return bfs(s,visited,0,minJump,maxJump);
+    }
+};
