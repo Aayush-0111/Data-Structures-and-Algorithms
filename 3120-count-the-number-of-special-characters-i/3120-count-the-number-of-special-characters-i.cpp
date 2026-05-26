702e2970566291e54c1a667feebfdc1b4db28929
Aayush Mishra <aayushmishra249@gmail.com>
2026-05-26 09:13:28 +0530

    Time: 0 ms (100%), Space: 9.4 MB (40.14%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,15 +1,18 @@
+bool frq1[26] = {false}, frq2[26] = {false};
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
         int n = word.size(), ans = 0;
         int frq1[26] = {0}, frq2[26] = {0};
         for(char& c : word){
-            if(c >= 'a' && c <= 'z') frq1[c-'a']++;
-            else frq2[c-'A']++;
+            if(c >= 'a' && c <= 'z' && !frq1[c-'a']) frq1[c-'a'] = true;
+            else if(c >= 'A' && c <= 'Z' && !frq2[c-'A']) frq2[c-'A'] = true;
         }
         for(int i = 0; i < 26; i++){
-            if(frq1[i] != 0 && frq2[i] != 0) ans++;
+            if(frq1[i] && frq2[i]) ans++;
         }
+        memset(frq1,false,26);
+        memset(frq2,false,26);
         return ans;
     }
 };
