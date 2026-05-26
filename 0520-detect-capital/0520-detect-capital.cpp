8ead0c852699c5e6a22330500737835ef49457ee
Aayush Mishra <aayushmishra249@gmail.com>
2026-05-26 10:08:31 +0530

    Time: 0 ms (100%), Space: 8 MB (79.61%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -3,26 +3,20 @@ public:
     bool detectCapitalUse(string word) {
         int n = word.size();
         if(n == 1) return true;
-        if(word[0] >= 'A' && word[0] <= 'Z'){
-            // all capital
-            if(word[1] >= 'A' && word[1] <= 'Z'){
-                for(int i = 2; i < n; i++){
-                    if(word[i] >= 'a' && word[i] <= 'z') return false;
-                }
-                return true;
-            }else{
-                // first capital
-                for(int i = 2; i < n; i++){
-                    if(word[i] >= 'A' && word[i] <= 'Z') return false;
-                }
-                return true;
-            }
-        }else{
-            // all lowercase
-            for(int i = 1; i < n; i++){
-                if(word[i] >= 'A' && word[i] <= 'Z') return false;
-            }
-            return true;
+        bool up = false, rest = false;
+        if(word[0] >= 'A' && word[0] <= 'Z') up = true; 
+        if(word[1] >= 'A' && word[1] <= 'Z'){
+            if(!up) return false;
+            rest = true;
         }
+        for(int i = 2; i < n; i++){
+            if(word[i] >= 'A' && word[i] <= 'Z'){
+                if(up && !rest || !up) return false;
+            }
+            else{
+                if(rest) return false;
+            }
+        }
+        return true;
     }
 };
