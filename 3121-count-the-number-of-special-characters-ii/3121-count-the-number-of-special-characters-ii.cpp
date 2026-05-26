c206d74979e2f8a0a60a7cc0b064f393f4d69673
Aayush Mishra <aayushmishra249@gmail.com>
2026-05-26 21:20:09 +0530

    Time: 70 ms (58.56%), Space: 24.4 MB (69.34%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,33 +1,25 @@
-bool f1[26] = {false}, f2[26] = {false};
-int flag[26] = {0};
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        int ans = 0;
-        for(char& c : word){
+        int n = word.size(), ans = 0;
+        vector<pair<int,int>> f(26,{-1,-1});
+        for(int i = 0; i < n; i++){
+            char c = word[i];
+            // first occurence of uppercase
             if(isupper(c)){
-                if(flag[c-'A'] == -1) continue;
-                f2[c-'A'] = true;
-                // uppercase comes first
-                if(!f1[c-'A']){
-                    flag[c-'A'] = -1;
-                }else if(f1[c-'A']){
-                    // uppercase comes after lowercase
-                    flag[c-'A'] = 1; 
+                if(f[c-'A'].first == -1){
+                    f[c-'A'].first = i;
                 }
             }else{
-                if(flag[c-'a'] == -1) continue;
-                f1[c-'a'] = true; 
-                // lowercase comes after uppercase
-                if(f2[c-'a']){
-                    flag[c-'a'] = -1;
-                }
+                // last occurence of lowercase
+                f[c-'a'].second = i;
             }
         }
-        for(int& i : flag) if(i == 1) ans++;
-        memset(flag,0,sizeof(flag));
-        memset(f1,false,sizeof(f1));
-        memset(f2,false,sizeof(f2));
+        for(int i = 0; i < 26; i++){
+            if(!(f[i].first == -1 || f[i].second == -1)){
+                if(f[i].first > f[i].second) ans++;
+            }
+        }
         return ans;
     }
 };
