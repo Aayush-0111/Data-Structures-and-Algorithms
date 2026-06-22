7417515c19a83e5dee14027f1c8a1abb17403b70
Aayush Mishra <aayushmishra249@gmail.com>
2026-06-22 22:45:02 +0530

    Time: 53 ms (75.75%), Space: 20.6 MB (73.88%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
@@ -1,8 +1,20 @@
+static long mod = 1e9+9;
 class Solution {
 public:
+    long powMod(long x,int y) {           
+        long res = 1;
+        while (y > 0) 
+        { 
+            if(y & 1) 
+                res = (res*x)%mod; 
+            y = y>>1;
+            x = (x*x)%mod;   
+        } 
+        return res; 
+    } 
     int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
         int n = s.size();
-        unordered_map<string,int> frq1; // for substring
+        unordered_map<long long,int> frq1; // for substring
         unordered_map<char,int> frq2;   // for unique chars
         // we choose the substring of length minsize
         // if we have a valid substring, we increment it's count in the map
@@ -13,16 +25,18 @@ public:
         // selected susbtring will contain the minsize substring and it will have atleast frequency
         // of bigger substring. And since, smaller substing have larger frequency, checking for minSize
         // substrings will suffice.
+        long long hash = 0, pow = powMod(89,minSize-1);
         int start = 0, ans = 0;
         for(int i = 0; i < n; i++){
             frq2[s[i]]++;
             if(i-start+1 > minSize){
-                frq2[s[start]]--;
-                if(frq2[s[start]] == 0) frq2.erase(s[start]);
+                hash -= (s[start]-'a')*pow;
+                if(--frq2[s[start]] == 0) frq2.erase(s[start]);
                 start++;
             }
+            hash = (hash*89%mod + s[i]-'a')%mod;
             if(i-start+1 == minSize && frq2.size() <= maxLetters){
-                ans = max(ans,++frq1[s.substr(start,i-start+1)]);
+                ans = max(ans,++frq1[hash]);
             }
         }
         return ans;
