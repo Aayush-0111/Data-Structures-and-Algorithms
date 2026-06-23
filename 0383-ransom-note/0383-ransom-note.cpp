2b1215b4f6eb6146f4ffc67eb317d24765d19e10
Aayush Mishra <aayushmishra249@gmail.com>
2026-06-23 12:14:00 +0530

    Time: 0 ms (100%), Space: 11.7 MB (88.86%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int frq1[26] = {0}, frq2[26] = {0};
+        for(char c : ransomNote) frq1[c-'a']++;
+        for(char c : magazine) frq2[c-'a']++;
+        for(int i = 0; i < 26; i++){
+            if(frq1[i] > frq2[i]) return false;
+        }
+        return true;
+    }
+};
