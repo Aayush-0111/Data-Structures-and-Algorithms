993892a0b6c983b4f2f8287b450e28b61d4adbb3
Aayush Mishra <aayushmishra249@gmail.com>
2026-06-26 12:32:33 +0530

    Time: 0 ms (100%), Space: 9.5 MB (49%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,34 +1,12 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int n = s.size();
-        bool flag = true;
-        for(int i = 1; i < n; i++){
-            if(s[i] == s[i-1]){
-                flag = false;
-                break;
-            }
-        }
-        if(flag) return 0;
-        string fin1 = "1";
-        string fin2 = "0";
-        for(int i = 1; i < n; i++){
-            if(fin1[i-1] == '1'){
-                fin1 += '0';
-            }else if(fin1[i-1] == '0'){
-                fin1 += '1';
-            }
-            if(fin2[i-1] == '1'){
-                fin2 += '0';
-            }else{
-                fin2 += '1';
-            }
-        }
-        int ops1 = 0, ops2 = 0;
+        int n = s.size(), flag = 1, ops = 0;
         for(int i = 0; i < n; i++){
-            if(fin1[i] != s[i]) ops1++;
-            if(fin2[i] != s[i]) ops2++;
+            int x = s[i]-'0';
+            if(x^flag) ops++;
+            flag = (flag) ? 0 : 1;
         }
-        return min(ops1,ops2);
+        return min(ops,n-ops);
     }
 };
