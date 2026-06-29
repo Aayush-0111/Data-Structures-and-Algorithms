c942312d25c3ba799c2731901d18866539ce1bb4
Aayush Mishra <aayushmishra249@gmail.com>
2026-06-30 00:31:20 +0530

    Time: 20 ms (79.75%), Space: 88.3 MB (72.78%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        int balance = 0, ans = 0;
+        unordered_map<int,int> mp;
+        // assign 0 -> -1 and 1 -> +1
+        // for i < j and balance at  i == balance at j
+        // means number of 1 and 0 b/w i & j is equal.
+        // cause balance represent prefix sum, so for sum to be equal
+        // for i and j, there has to be equal no. of 1 and -1 till j
+        // making the resulting sum = 0 and balance unchanged
+        mp[0] = -1;
+        for(int i = 0; i < n; i++){
+            balance += (nums[i] == 1) ? 1 : -1;
+            if(!mp.count(balance)) mp[balance] = i;
+            else ans = max(ans,i-mp[balance]);
+        }
+        return ans;
+    }
+};
