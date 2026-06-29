71ebe74a5b72521aa9efafbe7399dfce85c0ce5e
Aayush Mishra <aayushmishra249@gmail.com>
2026-06-30 00:32:05 +0530

    Time: 19 ms (86.31%), Space: 88.3 MB (72.78%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -10,7 +10,7 @@ public:
         // cause balance represent prefix sum, so for sum to be equal
         // for i and j, there has to be equal no. of 1 and -1 till j
         // making the resulting sum = 0 and balance unchanged
-        mp[0] = -1;
+        mp[0] = -1; // in case, answer array starts at 0th index.
         for(int i = 0; i < n; i++){
             balance += (nums[i] == 1) ? 1 : -1;
             if(!mp.count(balance)) mp[balance] = i;
