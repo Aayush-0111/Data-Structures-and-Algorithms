50afccec9d9131abd5e2be2a7b28a109eb833838
Aayush Mishra <aayushmishra249@gmail.com>
2026-06-29 10:46:24 +0530

    Time: 0 ms (100%), Space: 57.3 MB (4.48%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -2,11 +2,23 @@ class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         int n = arr.size();
-        sort(arr.begin(),arr.end());
-        arr[0] = 1;
-        for(int i = 1; i < n; i++){
-            if(arr[i] > arr[i-1]) arr[i] = arr[i-1]+1;
+        vector<int> count(n+1,0);
+        for(int& i : arr) count[min(i,n)]++;
+        int ans = 1;
+        for(int num = 2; num <= n; num++){
+            // ans+count[num] <= num ([1,2,3,100,100,100])
+            // for num = 100 and ans = 3, there 97 spots in the range [4,100], so we can't fill all the spots
+            // but we can just take all the occurenceso of 100(num) and reduce them to fill spots like 4,5,6....
+            
+            // ans + count[num] > num ([1,2,3,3,3,3])
+            // for num = 3 and ans = 2, we have more occurences of 3 than there are spots b/w 2 and 3.
+            // so our ans will just be num of this entire count[num] block ([3,3,3,3]).
+            
+            // both these conditions can be sumarized using this:
+            ans = min(ans+count[num],num); 
+            // as in 1st condition, ans is simply adding count[num] to the ans and in second condition
+            // ans is simply num. Which condition is valid is determined by whichever is lesser.
         }
-        return arr[n-1];
+        return ans;
     }
 };
