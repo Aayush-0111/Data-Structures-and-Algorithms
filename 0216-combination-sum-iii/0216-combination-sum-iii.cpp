3dcaaf5d2cd98f650bcb44c103972da9ac8a4d2d
Aayush Mishra <aayushmishra249@gmail.com>
2026-07-17 11:14:43 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.33%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,29 +1,22 @@
-vector<int> candidates = {1,2,3,4,5,6,7,8,9};
-const int sum = 45;
 class Solution {
 private:
-    void solve(vector<int>& candidates, int i, int k, int n, vector<int> op, vector<vector<int>>& ans, int count, const int sum,int currSum){
-        if(n == 0 && k == count){
+    void solve(int i, vector<int> op, vector<vector<int>>& ans, int n, int k){
+        if(n == 0 && op.size() == k){
             ans.push_back(op);
             return;
         }
-        currSum += (i < 9) ? candidates[i] : 0;
-        int remSum = n - currSum;
-        if(i >= candidates.size() || count > k || n < 0 || remSum > sum-currSum) return;
-        // include
-        op.push_back(candidates[i]);
-        solve(candidates,i+1,k,n-candidates[i],op,ans,count+1,sum,currSum);
-        op.pop_back();
-        currSum -= candidates[i];
-        //exclude
-        solve(candidates,i+1,k,n,op,ans,count,sum,currSum);
+        if(n < 0 || op.size() > k) return;
+        for(int j = i; j <= 9; j++){
+            op.push_back(j);
+            solve(j+1,op,ans,n-j,k);
+            op.pop_back();
+        }
     }
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
-        int i = 0, count = 0, currSum = 0;
         vector<int> op;
         vector<vector<int>> ans;
-        solve(candidates,i,k,n,op,ans,count,sum,currSum);
+        solve(1,op,ans,n,k);
         return ans;
     }
 };
