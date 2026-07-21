41a1e5cceb2d458354413f80590032d2a5f43c6a
Aayush Mishra <aayushmishra249@gmail.com>
2026-07-21 23:11:56 +0530

    Time: 63 ms (62.16%), Space: 54.9 MB (76.13%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int n = s.size(), l = 0, r = 0, m = 0, maxi = 0;
-        int ones = count(s.begin(),s.end(),'1');
+        int n = s.size(), l = 0, m = 0, maxi = 0, ones = 0;
         int i = 0;
         while(i < n){
             int start = i;
             while(i < n && s[i] == s[start]) ++i;
             if(s[start] == '0'){
                 ++m;
-                r = i-start;
-                maxi = (l != 0) ? max(maxi,r+l) : 0;
-                l = r;
-            }
+                maxi = (l != 0) ? max(maxi,i-start+l) : 0;
+                l = i-start;
+            }else ones += (i-start);
         }
         if(m < 2) return ones;
         return ones + maxi;
