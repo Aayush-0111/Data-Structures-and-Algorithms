8e848d397bc4310a3d5b1377824c2f70f4c92a89
Aayush Mishra <aayushmishra249@gmail.com>
2026-07-28 10:01:13 +0530

    Time: 20 ms (88.79%), Space: 54.3 MB (85.71%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -2,10 +2,19 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         int n = s.size();
-        int partition = n/2;
-        sort(s.begin(),s.begin()+partition);
-        for(int i = 0; i < partition; i++){
-            s[n-i-1] = s[i];
+        int frq[26] = {0};
+        // count frequencies of characters in one half.
+        for(int i = 0; i < n/2; i++) ++frq[s[i]-'a'];
+        int l = 0, r = n-1;
+        // put lexicographically smallest characters in both halfs to obtain
+        // lexicographically smallest palindromic permutation.
+        for(int i = 0; i < 26; i++){
+            while(frq[i] > 0){
+                char c = i + 'a';
+                s[l++] = c;
+                s[r--] = c;
+                --frq[i];
+            }
         }
         return s;
     }
