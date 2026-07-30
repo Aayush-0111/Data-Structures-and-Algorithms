5331018e3c8c80c1a016d8259bda999b2d4ff6e7
Aayush Mishra <aayushmishra249@gmail.com>
2026-07-30 20:16:10 +0530

    Time: 5 ms (79.77%), Space: 25.1 MB (96.13%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -5,11 +5,10 @@ public:
         memset(frq,0,sizeof(frq));
         for(char& c : word) ++frq[c-'a'];
         sort(frq,frq+26);
-        int cnt = 0, factor = 1, ans = 0;
+        int cnt = 0, ans = 0;
         for(int i = 25; i >= 0; i--){
-            ans += frq[i]*factor;
+            ans += frq[i]*(1 + cnt/8);
             ++cnt;
-            if(cnt%8 == 0) ++factor;
         }
         return ans;
     }
