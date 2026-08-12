213db41f55192616692a71b3e408669777f9f4f8
Aayush Mishra <aayushmishra249@gmail.com>
2026-08-13 00:12:39 +0530

    Time: 39 ms (99.89%), Space: 155.8 MB (5.11%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
-        int n = nums.size();
-        int ans{0}, l{0};
-        unordered_map<int,pair<vector<int>,int>> frq;
-        // populate the map with positon of elements in the array.
-        for(int i{n-1}; i >= 0; --i) frq[nums[i]].first.push_back(i);
-        for(int r{0}; r < n; ++r){
+        int n{(int)nums.size()}, l{0}, ans{0};
+        unordered_map<int,int> frq;
+        frq.reserve(n);
+        for(int r = 0; r < n; ++r){
             int x = nums[r];
-            ++frq[x].second;
-            if(frq[x].second > k){
-                //l = frq[x].first.back() + 1; will not work cause when we
-                // move l, we are not reducing the frequency of the elements
-                // lying b/w old l and new l, so it might trigger the condition (>k)
-                // for the elements which are not be in the window anymore.
-                // ex:{1,2,2,1} here when considering last '1', count will still
-                // consider the first '1' cause we have not reduced it's count.
-                // so we are essentially taking the l backwards which is not possible
-                // in a sliding window operation.
-                l = max(l,frq[x].first.back()+1);  // kind of lazy cleanup preventing stale cleanup, making sure l does not go backwards.
-                frq[x].first.pop_back();
-                --frq[x].second;
-            }
+            auto it = frq.find(x);
+            int& f = (it == frq.end()) ? frq[x] = 1 : ++(it->second);
+            while(f > k) --frq[nums[l++]];
             ans = max(ans,r-l+1);
         }
         return ans;
