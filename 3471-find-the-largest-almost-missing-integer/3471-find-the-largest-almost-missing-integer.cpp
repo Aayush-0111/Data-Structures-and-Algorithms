f9d6eb8306a2670a2936820af40f4829667a9510
Aayush Mishra <aayushmishra249@gmail.com>
2026-08-18 13:51:21 +0530

    Time: 0 ms (100%), Space: 28.7 MB (95.49%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,20 +1,19 @@
-static int frq1[51];
+static int frq[51];
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
+        memset(frq,0,sizeof(frq));
         int n{(int)nums.size()}, ans{-1};
-        static int frq2[51] = {0};
-        memset(frq1,0,sizeof(frq1));
-        for(int i{0}; i+k <= n; ++i){
-            int l{i};
-            while(l < i+k){
-                frq2[nums[l]] = 1;
-                ++l;
-            }
-            for(int j{0}; j <= 50; ++j) frq1[j] += frq2[j];
-            memset(frq2,0,sizeof(frq2));
+        for(int& i : nums) ++frq[i];
+        if(k == 1){
+            for(int i{50}; i >= 0; --i) if(frq[i] == 1) return i;
+        }else if(k == n){
+            return *max_element(nums.begin(),nums.end());
+        }else{
+            if(frq[nums[0]] == 1 && frq[nums[n-1]] != 1) return nums[0];
+            else if(frq[nums[0]] != 1 && frq[nums[n-1]] == 1) return nums[n-1];
+            else if(frq[nums[0]] == 1 && frq[nums[n-1]] == 1) return max(nums[0],nums[n-1]);
         }
-        for(int i{0}; i <= 50; ++i) if(frq1[i] == 1) ans = i;
-        return ans;
+        return -1;
     }
 };
