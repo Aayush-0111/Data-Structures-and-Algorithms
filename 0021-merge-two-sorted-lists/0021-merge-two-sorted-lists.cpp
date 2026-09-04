d56e17634ec6b79f0af3d77f3f0b45306781b62f
Aayush Mishra <aayushmishra249@gmail.com>
2026-09-04 17:40:19 +0530

    Time: 0 ms (100%), Space: 19.5 MB (27.61%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    void merge(ListNode *l1, ListNode *l2){
+        if(!l1->next){
+            l1->next = l2;
+            return;
+        }
+        ListNode *curr1 = l1, *next1 = l1->next;
+        ListNode *curr2 = l2, *next2 = l2->next;
+        while(next1 && curr2){
+            if((curr1->val <= curr2->val) && (curr2->val <= next1->val)){
+                curr1->next = curr2;
+                next2 = curr2->next;
+                curr2->next = next1;
+                curr1 = curr2;
+                curr2 = next2;
+            }else{
+                curr1 = next1;
+                next1 = curr1->next;
+                if(!next1){
+                    curr1->next = curr2;
+                    return;
+                }
+            }
+        }
+    }
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1) return list2;
+        if(!list2) return list1;
+        if(list1->val <= list2->val) merge(list1,list2);
+        else merge(list2,list1);
+        return (list1->val <= list2->val) ? list1 : list2;
+    }
+};
