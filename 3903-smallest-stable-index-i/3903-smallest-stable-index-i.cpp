53dd391da4c89bac7fb77900e7ded21cb69ad91f
Aayush Mishra <aayushmishra249@gmail.com>
2026-09-04 12:09:00 +0530

    Time: 3 ms (25.17%), Space: 31 MB (72.92%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -5,14 +5,11 @@ public:
         vector<int> suff(n,0);
         suff[n-1] = nums[n-1];
         for(int i{n-2}; i >= 0; --i) suff[i] = min(nums[i],suff[i+1]);
-        int pre{INT_MIN}, stable{INT_MAX};
+        int pre{INT_MIN};
         for(int i{0}; i < n; ++i){
             pre = max(pre,nums[i]);
-            if(pre-suff[i] <= k) {
-                stable = i;
-                break;
-            }
+            if(pre-suff[i] <= k) return i;
         }
-        return (stable != INT_MAX) ? stable : -1;
+        return -1;
     }
 };
