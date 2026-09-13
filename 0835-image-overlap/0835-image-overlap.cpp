1085458d8f3d1ed0c2e1842525ff9007ccc35dd2
Aayush Mishra <aayushmishra249@gmail.com>
2026-09-13 20:54:59 +0530

    Time: 8 ms (94.39%), Space: 13.7 MB (52.91%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    struct pair_hash {
-        size_t operator()(const pair<int, int>& p) const {
-            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
-        }
-    };
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         int n = img1.size();
         int ans{0};
-        vector<pair<int,int>> im1, im2;
+        vector<int> im1, im2;
         for(int i{0}; i < n; ++i){
             for(int j{0}; j < n; ++j){
-                if(img1[i][j] == 1) im1.push_back({i,j});
-                if(img2[i][j] == 1) im2.push_back({i,j});
+                if(img1[i][j] == 1) im1.emplace_back(i*n+j); // can retrieve the idxs like i->i/n and j->j%n
+                if(img2[i][j] == 1) im2.emplace_back(i*n+j);
             }
         }
-        unordered_map<pair<int,int>,int, pair_hash> offset;
-        for(auto& p : im1){
-            for(auto& q : im2){
-                const pair<int,int> offs = {p.first-q.first,p.second-q.second};
-                ans = max(ans,++offset[offs]);
+        // offsets range from -(n-1) to (n-1) so we can use a vector instead of a map
+        vector<vector<int>> offset(2*n-1,vector<int>(2*n-1,0));
+        for(int& p : im1){
+            for(int& q : im2){
+                int dx{p/n - q/n}, dy{p%n - q%n};
+                // shifting negative idx to positive
+                dx += n-1;
+                dy += n-1;
+                ans = max(ans,++offset[dx][dy]);
             }
         }
         return ans;
