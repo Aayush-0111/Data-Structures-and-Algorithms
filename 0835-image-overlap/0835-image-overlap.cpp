65f9328cfcec9c1c53d278e65a9ed470653516bd
Aayush Mishra <aayushmishra249@gmail.com>
2026-09-13 20:40:18 +0530

    Time: 135 ms (29.37%), Space: 16.4 MB (6.5%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    struct pair_hash {
+        size_t operator()(const pair<int, int>& p) const {
+            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+        }
+    };
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         int n = img1.size();
         int ans{0};
@@ -10,7 +15,7 @@ public:
                 if(img2[i][j] == 1) im2.push_back({i,j});
             }
         }
-        map<pair<int,int>,int> offset;
+        unordered_map<pair<int,int>,int, pair_hash> offset;
         for(auto& p : im1){
             for(auto& q : im2){
                 const pair<int,int> offs = {p.first-q.first,p.second-q.second};
