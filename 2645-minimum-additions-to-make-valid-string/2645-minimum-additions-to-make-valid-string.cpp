c59c0387327d090f1f4361c002a1825857afb108
Aayush Mishra <aayushmishra249@gmail.com>
2026-09-18 16:12:22 +0530

    Time: 0 ms (100%), Space: 9.4 MB (53.12%) - LeetHub

--- 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int addMinimum(string word) {
         int n = word.size();
-        int count{1};
-        // every imbalance accounts for a new "abc" group. if we wind out how many groups of "abc" there are,
-        // we can count the minimum number of letters by subtracting word.size() from total number of characters
-        // "abc" -> 3 chars so -> 3*(no. of groups) - word.size() is our answer.
-        for(int i{1}; i < n; ++i) if(word[i] <= word[i-1]) ++count;
-        return 3*count - n;
+        int i{0}, j{0}, ans{0};
+        while(i < n || j%3 != 0){
+            char want = 'a' + j%3;
+            if(word[i] == want) ++i;
+            else ++ans;
+            ++j;
+        }
+        return ans;
     }
 };
