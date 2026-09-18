6cf889d7417e6468efef7a7d6076f8dffd62e7d6
Aayush Mishra <aayushmishra249@gmail.com>
2026-09-18 15:58:51 +0530

    Time: 0 ms (100%), Space: 9.3 MB (78.98%) - LeetHub

--- 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
@@ -2,23 +2,11 @@ class Solution {
 public:
     int addMinimum(string word) {
         int n = word.size();
-        int cnt{0};
-        for(int i{0}; i < n; ++i){
-            if(word[i] == 'a'){
-                if(i == n-1 || (i == 0 && i == n-1)) cnt += 2;
-                if(i+1 < n && word[i+1] == 'a') cnt += 2;
-                else if(i+1 < n && word[i+1] == 'c') ++cnt;
-            }else if(word[i] == 'b'){
-                if(i == 0) ++cnt;
-                if(i+1 < n && word[i+1] == 'b') cnt += 2;
-                else if(i+1 < n && word[i+1] == 'a') ++cnt;
-                if(i+1 == n) ++cnt;
-            }else{
-                if(i == 0) cnt += 2;
-                if(i+1 < n && word[i+1] == 'c') cnt += 2;
-                else if(i+1 < n && word[i+1] == 'b') ++cnt;
-            }
-        }
-        return cnt;  
+        int count{1};
+        // every imbalance accounts for a new "abc" group. if we wind out how many groups of "abc" there are,
+        // we can count the minimum number of letters by subtracting word.size() from total number of characters
+        // "abc" -> 3 chars so -> 3*(no. of groups) - word.size() is our answer.
+        for(int i{1}; i < n; ++i) if(word[i] <= word[i-1]) ++count;
+        return 3*count - n;
     }
 };
